f2a05eecd5c982b1a11b03f9981a17d42eb177f6
coledockter <cdockter@telegeography.com>
2024-04-29 19:23:28 -0500

    Register & Login DB functionality implemented.

--- app.py
+++ app.py
@@ -1,73 +1,102 @@
+import os
 from datetime import datetime
 from urllib import request
-from flask import Flask, render_template, request, session, flash, redirect, url_for
+from flask import Flask, render_template, request, session, flash, redirect, url_for, abort
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy.exc import SQLAlchemyError
+
+from db.models import db, Users, Pto
+from db.session import generate_db_conn_string
 from jinja2 import Template
 
-app = Flask(__name__)
 
-# Manually generate app secret key? Read about this more...
-app.secret_key = "1awawet-kwetkmKEJTKET868-j-rP_weroWEOTIulkm"
+def create_app():
+    app = Flask(__name__)
 
-users = {'cole@gmail.com': 12345,
-         'pto_balance': 5}
-@app.route('/', methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        pto_mod = request.form.get("addhours")
-        formatted_date = datetime.today().strftime("%Y-%m-%d")
-        # Database modification here
-        print(pto_mod, formatted_date)
+    app.config['SQLALCHEMY_DATABASE_URI'] = generate_db_conn_string()
+    db.init_app(app)
+
+    # Manually generate app secret key? Read about this more...
+    app.secret_key = os.environ['SECRET_KEY']
+
+    # Creates our tables in the DB
+    with app.app_context():
+        db.create_all()
+
+    users = {}
+
+    @app.route('/', methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            pto_mod = request.form.get("addhours")
+            formatted_date = datetime.today().strftime("%Y-%m-%d")
+            # Database modification here
+            print(pto_mod, formatted_date)
+            if session.get("email"):
+                # Add pto balance to params here, pass in on html file.
+                users['pto_balance'] += pto_mod
+            else:
+                # If user is not logged in, what do we do with form input?
+                flash("You must be logged in to do that!")
+
         if session.get("email"):
-            # Add pto balance to params here, pass in on html file.
-            users['pto_balance'] += pto_mod
+            return render_template("home.html", email=session.get("email"), pto=session.get("pto_balance"))
         else:
-            # If user is not logged in, what do we do with form input?
-            flash("You must be logged in to do that!")
-    if session.get("email"):
-        return render_template("home.html", email=session.get("email"), pto=session.get("pto_balance"))
-    else:
-        return render_template("home.html", email="Guest", pto="?")
+            return render_template("home.html", email="Guest", pto="?")
 
+    @app.route('/login/', methods=["GET", "POST"])
+    def login():
+        if request.method == "POST":
+            email = request.form.get("email")
+            password = request.form.get("password")
 
+            # Query the user
+            user = db.session.query(Users).filter(Users.email == email).first()
 
-@app.route('/login/', methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = request.form.get("password")
-        # print(email, password)
-        # Check if our email & password match a record in our db. Verifying the password passed in from form with the properties from when it was hashed in DB.
-        if pbkdf2_sha256.verify(password, users.get(email)):
-            # Lets our browser know we have a valid and logged in user.
+            # Check if our email & password match a record in our db. Verifying the password passed in from form with the properties from when it was hashed in DB.
+            if pbkdf2_sha256.verify(password, user.password):
+                # Lets our browser know we have a valid and logged in user.
+                session["email"] = email
+
+                # Grab PTO balance from the PTO table.
+                pto = db.session.query(Pto).filter(Pto.user_id == user.id and Pto.is_current is True).first()
+                session["pto_balance"] = pto.pto_balance
+
+                flash("Login Successful!")
+                return redirect(url_for('home'))
+            else:
+                flash("Login Failed. Please try again.")
+
+        return render_template("auth/login.html")
+
+    @app.route('/register/', methods=["GET", "POST"])
+    def register():
+        if request.method == "POST":
+            email = request.form.get("email")
+            password = pbkdf2_sha256.hash(request.form.get("password"))
+            pto_balance = request.form.get("pto_balance")
+            is_senior = request.form.get("is_senior")
+
+            # save to DB or users dict
+            user = Users(email=email, password=password, is_senior=is_senior)
+            try:
+                db.session.add(user)
+                db.session.commit()
+                # Need to commit the user before PTO, so we can setup the foreign key constraint.
+                pto = Pto(pto_balance=pto_balance, user_id=user.id, last_updated=datetime.now(), is_current=True)
+                db.session.add(pto)
+                db.session.commit()
+            except SQLAlchemyError as e:
+                # Add functionality for duplicate emails later?
+                abort(500, message="An error occurred while registering the user.")
+
+            # Lets our browser know we have a valid and logged in user. Displayed on home page.
             session["email"] = email
-            # pull this from database.
-            # session['pto_balance'] = pto_balance
+            session["pto_balance"] = pto_balance
 
-            flash("Login Successful!")
-            return redirect(url_for('home'))
-        else:
-            flash("Login Failed. Please try again.")
+            flash("Registration Successful!")
+            return redirect(url_for('login'))
 
-    return render_template("auth/login.html")
+        return render_template("auth/register.html")
 
-
-@app.route('/register/', methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        email = request.form.get("email")
-        password = pbkdf2_sha256.hash(request.form.get("password"))
-        pto_balance = request.form.get("pto_balance")
-
-        # save to DB or users dict
-        users[email] = password
-        users[pto_balance] = pto_balance
-
-        # Lets our browser know we have a valid and logged in user.
-        session["email"] = email
-        session["pto_balance"] = pto_balance
-
-        flash("Registration Successful!")
-        return redirect(url_for('login'))
-
-    return render_template("auth/register.html")
+    return app
--- db/models.py
+++ db/models.py
@@ -5,20 +5,20 @@ from sqlalchemy.orm import backref
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Users(db.Model):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
-    is_senior = Column(Boolean, nullable=False)
+    is_senior = Column(Integer, nullable=False, default=0)
 
 
 class Pto(db.Model):
     __tablename__ = "pto"
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, db.ForeignKey("user.id"))
+    user_id = Column(Integer, db.ForeignKey("users.id"))
     # This sets up a foreign key relationship with our users.
-    user = db.relationship("User", backref=backref("pto", lazy="dynamic"))
+    user = db.relationship("Users", backref=backref("pto"))
     pto_balance = Column(Float, nullable=False, default=0)
     last_updated = Column(DateTime, nullable=False)
     is_current = Column(Boolean, nullable=False, default=False)
