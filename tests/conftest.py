13f1ec3b657d4c1622b86dbbcba4750f89b7665c
coledockter <cdockter@telegeography.com>
2024-05-14 15:30:14 -0500

    Adding init for testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,21 +2,21 @@ import os
 import tempfile
 
 import pytest
-from flask import create_app
-from flask.db import get_db, init_db
-
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+from app import create_app
+from db.models import db
 
 
+# Following link Here: https://flask.palletsprojects.com/en/3.0.x/tutorial/tests/
+
+#Authentication
 class AuthActions(object):
     def __init__(self, client):
         self._client = client
 
-    def login(self, username='test', password='test'):
+    def login(self, email='test', password='test'):
         return self._client.post(
             '/auth/login',
-            data={'username': username, 'password': password}
+            data={'email': email, 'password': password}
         )
 
     def logout(self):
@@ -27,19 +27,28 @@ class AuthActions(object):
 def auth(client):
     return AuthActions(client)
 
-# Following link Here: https://flask.palletsprojects.com/en/3.0.x/tutorial/tests/
+
 @pytest.fixture
 def app():
     db_fd, db_path = tempfile.mkstemp()
-
     app = create_app({
         'TESTING': True,
-        'DATABASE': db_path,
     })
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://' + db_path
 
     with app.app_context():
-        init_db()
-        get_db().executescript(_data_sql)
+        db.create_all()
+        db.engine.execute("""
+        INSERT INTO users (email, password, is_senior)
+        VALUES
+            ('test@test.com', 'pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', 1),
+            ('other@other.com', 'pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', 0);
+
+        INSERT INTO pto (user_id, pto_balance, last_updated, is_current)
+        VALUES
+            (1, 25 || -5 || 0, '2018-01-01 00:00:00', true);
+            (2, 25 || -5 || 0, '2019-01-01 00:00:00', false);
+        """)
 
     yield app
 
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,18 @@
+import pytest
+from flask_sqlalchemy import SQLAlchemy
+from db.models import db
+
+
+def test_get_close_db(app):
+    with app.app_context():
+        assert db is SQLAlchemy()
+
+    with pytest.raises(SQLAlchemy.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    assert 'closed' in str(e.value)
+
+
 def test_init_db_command(runner, monkeypatch):
     class Recorder(object):
         called = False
@@ -5,7 +20,7 @@ def test_init_db_command(runner, monkeypatch):
     def fake_init_db():
         Recorder.called = True
 
-    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
-    result = runner.invoke(args=['init-db'])
+    monkeypatch.setattr('SQLALCHEMY().db.create_all()', fake_init_db)
+    result = runner.invoke(args=['create-all'])
     assert 'Initialized' in result.output
     assert Recorder.called
